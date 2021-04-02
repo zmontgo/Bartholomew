128afe7550c63d341c7bc8a53aef41e094926b09
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-04-02 09:58:17 -0500

    More testing

--- eventActions/welcomeActions.js
+++ eventActions/welcomeActions.js
@@ -3,7 +3,7 @@ const config = require('../config.json');
 
 class welcomeActions {
   static async channelWelcome(client, oldMember, newMember) {
-    console.log(newMember.roles.cache.has(role => role.id === config.roles.member));
+    console.log(newMember.roles.cache.has(role => {console.log(role.id);role.id === config.roles.member}));
     console.log(!oldMember.roles.cache.has(role => role.id === config.roles.member));
     if (newMember.roles.cache.has(role => role.id === config.roles.member) && !oldMember.roles.cache.has(role => role.id === config.roles.member)) {
       const reactrole = newMember.guild.roles.cache.find(role => role.id === config.roles.reactDivider);
