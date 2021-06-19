4445ca194f7faaab161322798ba095287f809aa4
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-06-19 10:41:22 -0500

    Prevent double firing for starboard

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -3,15 +3,8 @@ const starboardActions = require('../eventActions/starboardActions');
 const reportActions = require('../eventActions/reportAction');
 
 module.exports = async (client, reaction, user) => {
-	try {
-		// When we receive a reaction we check if the reaction is partial or not
-		if (reaction.partial) {
-			// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-			reaction = await reaction.fetch();
-		}
-	} catch (error) {
-		console.error('Something went wrong when fetching the message: ', error);
-		// Return as `reaction.message.author` may be undefined/null
+	// When we receive a reaction we check if the reaction is partial or not, and return because this event will be fired by the raw event
+	if (reaction.partial) {
 		return;
 	}
   
