c24d4767d4ef67452642643a86077bfa238bd0d9
Zachary Montgomery <zachmontgo@iodinedev.com>
2022-07-07 10:14:37 -0500

    Add easter eggs

--- src/eventActions/countingActions.ts
+++ src/eventActions/countingActions.ts
@@ -14,7 +14,10 @@ export class countingActions {
       const lastNumber = await this.getLatest(message.guildId, false);
 
       if (isNaN(number) || number < 1) {
-        const next = await this.killCount(-1, message);
+        const [next, killed] = await this.killCount(-1, message);
+
+        if (!killed) return;
+
         return await message.channel.send(
           `Oops! Looks like <@${message.author.id}> reset the count by sending invalid text! The next number is **${next}**!`
         );
@@ -22,18 +25,24 @@ export class countingActions {
 
       try {
         if (lastNumber && lastNumber.next !== number) {
-          const next = await this.killCount(number, message);
+          const [next, killed] = await this.killCount(number, message);
+
+          if (!killed) return;
+
           return await message.channel.send(
             `Oops! Looks like <@${message.author.id}> reset the count by breaking the streak! The next number is **${next}**!`
           );
         }
 
-        if (lastNumber && lastNumber.user === message.author.id) {
-          const next = await this.killCount(number, message);
-          return await message.channel.send(
-            `Oops! Looks like <@${message.author.id}> reset the count by sending two numbers in a row! The next number is **${next}**!`
-          );
-        }
+        // if (lastNumber && lastNumber.user === message.author.id) {
+        //   const [next, killed] = await this.killCount(number, message);
+        // 
+        //   if (!killed) return;
+        // 
+        //   return await message.channel.send(
+        //     `Oops! Looks like <@${message.author.id}> reset the count by sending two numbers in a row! The next number is **${next}**!`
+        //   );
+        // }
       } catch {}
 
       await this.putLatest(
@@ -48,15 +57,35 @@ export class countingActions {
       if (number % 1000 === 0) return await message.react("🌠");
       if (number % 100 === 0) return await message.react("💯");
       await message.react("✅");
-      return await this.typeNumber(client, message, number);
+      return await this.easterEgg(client, message, number);
+    }
+  }
+
+  static async easterEgg(client, message, number) {
+    const count = await this.countValid(message.author.id, message.guildId);
+    if (count > 25) {
+      const random = getRandom(0, 1000);
+      const textChannel = await message.guild.channels.cache.get(config.channels.countingMessages);
+
+      if (random < 10) {
+        this.typeNumber(client, message, number);
+      } else if (random >= 10 && random < 15) {
+        await message.author.send(`Good job. Looks like you've send ${count} numbers before. Valid numbers, anyways. I forgot where I left the paper counting your wrong numbers and frankly am just too tired to find that right now.`)
+      } else if (random >= 15 && random < 16) {
+        await message.author.send(`Wow. Incredible. You've posted ${count} numbers. I think it's finally time to tell you a secret... the cult of 5040. Don't tell anyone, but it's been slowly growing this whole time. Every time someone posts a multiple of 5040, react to it with :thought_balloon:. Why?\n5040 is a factorial (7!), a superior highly composite number, abundant number, colossally abundant number and the number of permutations of 4 items out of 10 choices (10 × 9 × 8 × 7 = 5040). It is also one less than a square, making (7, 71) a Brown number pair. Also it's Plato's favorite number. Do you need any more reason to join? So start doing that, and if anyone asks, tell them nothing.`)
+      } else if (random >= 16 && random < 20) {
+        await textChannel.send("Just watching the count go up... :eyes:")
+      } else if (random >= 20 && random < 30) {
+        await textChannel.send("*Clever remark*")
+      } else if (random >= 30 && random < 40) {
+        await textChannel.send("Yes... keep counting... not harvesting data over here for nefarious *machine learning* purposes.")
+      }
     }
   }
 
   static async typeNumber(client, message, number) {
-    if (getRandom(0, 100) > 99) {
-      const increaseMessage = await message.channel.send(`${number + 1}`);
-      return await this.checkNumber(client, increaseMessage);
-    }
+    const increaseMessage = await message.channel.send(`${number + 1}`);
+    return await this.checkNumber(client, increaseMessage);
   }
 
   static async mendBroken(client, message, action) {
@@ -67,6 +96,7 @@ export class countingActions {
         where: {
           server: message.guildId,
           message: message.id,
+          broke: false
         },
         orderBy: {
           date: "asc",
@@ -107,7 +137,7 @@ export class countingActions {
       next = await this.findNext(user, server, lastNumber);
     }
 
-    await prisma.count.create({
+    const record = await prisma.count.create({
       data: {
         number: number > Number.MAX_SAFE_INTEGER ? -1 : number,
         user: user,
@@ -118,7 +148,7 @@ export class countingActions {
       },
     });
 
-    return next;
+    return [next, record];
   }
 
   static async findNext(user, server, lastNumber) {
@@ -152,17 +182,63 @@ export class countingActions {
     return next < 1 ? 1 : next;
   }
 
+  static async countValid(user, server) {
+    return await prisma.count.count({
+      where: {
+        user: user,
+        server: server,
+        broke: false
+      }
+    })
+  }
+
   static async killCount(number, message) {
-    const next = await this.putLatest(
+    const [next, latest] = await this.putLatest(
       number,
       message.author.id,
       message.id,
       message.guildId,
       true
     );
+    
+    const count = await this.countValid(message.author.id, message.guildId);
+    if (count > 50) {
+      const random = getRandom(0, 10000);
+      const textChannel = await message.guild.channels.cache.get(config.channels.countingMessages);
+
+      if (random < 10 && random > 2) {
+        await textChannel.send(`Hey, just a reminder, <@${message.author.id}>, this channel is for counting *up*. :\\)`)
+      } else if (random <= 2) {
+        await prisma.count.delete({
+          where: {
+            id: latest.id
+          }
+        })
+
+        const lastNumber = await this.getLatest(message.guildId, false);
+
+        await message.delete();
+
+        await textChannel.send(`Really <@${message.author.id}>? You broke the streak *again*? I think we should just collectively ignore that.${(lastNumber && lastNumber.next) ? " The next number is " + lastNumber.next + " everyone!": ""} `)
+
+        return [next, false];
+      } else if (random >= 10 && random <= 40) {
+        await textChannel.send(`Hey! Just a friendly reminder that your mistakes are permanent, <@${message.author.id}>.`)
+      } else if (random > 100 && random <= 200) {
+        await textChannel.send(`And here I was, thinking you knew what you were doing, <@${message.author.id}>.`)
+      } else if (random > 200 && random <= 250) {
+        await textChannel.send(`*Developer note: remember to insert sarcastic comment about <@${message.author.id}>'s performance here.*`)
+      } else if (random > 250 && random <= 300) {
+        await textChannel.send(`Hey, <@${message.author.id}>. Constructive feedback: wrong number.`)
+      } else if (random > 250 && random <= 300) {
+        await textChannel.send(`<@${message.author.id}>. What would Diophantus think?`)
+      } else if (random > 300 && random <= 350) {
+        await textChannel.send(`<@${message.author.id}>, did you do that just so that I would say something witty? Well, I'm not, and I'm deducting points from your score for that.`)
+      }
+    } 
 
     await message.react("❌");
 
-    return next;
+    return [next, true];
   }
 }
